d6c7a5d56b2383cd6863d750437acc92e7df7291
nikitaPapok <papokmikita@gmail.com>
2025-04-27 15:33:59 +0400

    feat: implement function that creates new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  let fileExists;
+  const filePath = path.join(process.cwd(), "src", "fs", "files/fresh.txt");
+  const data = "I am fresh and young";
+  fs.exists(filePath, (exists) => {
+    if (exists) {
+      throw new Error("FS operation failed");
+    }
+    fs.writeFile(filePath, data, (err) => {
+      console.error({ err });
+    });
+  });
+
+  // Write your code here
 };
 
 await create();
