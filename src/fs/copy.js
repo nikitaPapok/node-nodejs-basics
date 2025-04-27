5843ca3488dbc4f0e495218329e80e62f88d5e8d
nikitaPapok <papokmikita@gmail.com>
2025-04-27 22:36:52 +0400

    feat: implement function that copies folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
 const copy = async () => {
-    // Write your code here 
+  const originalFolderPath = path.join(process.cwd(), "src", "fs", "files");
+  const copiedFoldersPath = path.join(process.cwd(), "src", "fs", "files_copy");
+  const isCopiedFolderExist = fs.existsSync(copiedFoldersPath);
+  if (isCopiedFolderExist) {
+    throw new Error("FS operation failed");
+  }
+  fs.mkdirSync(copiedFoldersPath);
+  fs.readdir(originalFolderPath, (err, files) => {
+    for (const currentFile of files) {
+      const data = fs.readFileSync(
+        `${originalFolderPath}/${currentFile}`,
+        "utf-8"
+      );
+      fs.writeFile(`${copiedFoldersPath}/${currentFile}`, data, (err) => {
+        console.log(err);
+      });
+    }
+  });
 };
 
 await copy();
